108e928d0a3947c0d487e98bf0583649a8afad64
mumarch <umarch.skku@gmail.com>
2024-08-28 17:06:20 +0500

    Add files via upload

---
+++ app4.py
@@ -0,0 +1,40 @@
+# numpy version:1.26.4 is compatable with pandasai
+# pip install numpy==1.26.4  pyyaml
+
+import pandas as pd
+from pandasai.llm.openai import OpenAI
+import streamlit as st # type: ignore
+#import numpy as np
+
+#from pandasai.llm.local_llm import LocalLLM
+from pandasai import SmartDataframe
+
+
+
+st.title("Data Analysis using Prompts")
+
+OPENAI_API_KEY = st.text_input("Enter your OpenAI Api Key")
+
+model = OpenAI(api_token=OPENAI_API_KEY)
+
+uploaded_file = st.file_uploader("Upload a CSV file", type=['csv'])
+
+# reading the uploaded dataset
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data.head(2))
+
+    df = SmartDataframe(data, config={"llm":model})
+
+    prompt = st.text_area("Enter your prompt")
+    
+    if st.button("Generate"):
+        if prompt:
+            with st.spinner("Generating response..."):
+                st.write(df.chat(prompt))
+                
+
+
+
+
+
