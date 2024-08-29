1f0fc2e487499e62bc0e37a7baad465399ed277b
mumarch <umarch.skku@gmail.com>
2024-08-29 16:27:01 +0500

    Update app4.py

--- app4.py
+++ app4.py
@@ -1,10 +1,9 @@
 # numpy version:1.26.4 is compatable with pandasai
 # pip install numpy==1.26.4  pyyaml
 
-#app4.1 version at local disk
-
 import pandas as pd
 from pandasai.llm.openai import OpenAI
+import openai
 import streamlit as st # type: ignore
 #import numpy as np
 
@@ -18,24 +17,26 @@ st.title("Data Analysis using Prompts")
 api_key = st.text_input("Enter your OpenAI Api Key")
 
 if st.button("Submit"):
-    OPENAI_API_KEY = api_key
-    model = OpenAI(api_token=OPENAI_API_KEY)
-
-    uploaded_file = st.file_uploader("Upload a CSV file", type=['csv'])
-
-    # reading the uploaded dataset
-    if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file)
-        st.write(data.head(2))
-
-        df = SmartDataframe(data, config={"llm":model})
-
-        prompt = st.text_area("Enter your prompt")
+    #OPENAI_API_KEY = api_key
+    openai.api_key = api_key
     
-        if st.button("Generate"):
-            if prompt:
-                with st.spinner("Generating response..."):
-                    st.write(df.chat(prompt))
+model = OpenAI(api_token=openai.api_key)
+
+uploaded_file = st.file_uploader("Upload a CSV file", type=['csv'])
+
+# reading the uploaded dataset
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data.head(2))
+
+    df = SmartDataframe(data, config={"llm":model})
+
+    prompt = st.text_area("Enter your prompt")
+    
+    if st.button("Generate"):
+        if prompt:
+            with st.spinner("Generating response..."):
+                st.write(df.chat(prompt))
                 
 
 
